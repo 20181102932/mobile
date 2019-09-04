9326ea486c2d795d3fa7bc6561ed9ca0ab54ccc6
20181102932 <908907990@qq.com>
2019-09-04 11:59:43 +0800

    yonghuxitong

--- mobile/main.swift
+++ mobile/main.swift
@@ -1,12 +1,16 @@
-//
-//  main.swift
-//  mobile
-//
-//  Created by s20181102932 on 2019/9/4.
-//  Copyright © 2019 wjx. All rights reserved.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+print("请输入用户名\n")
+var name = readLine()!;
+print("请输入密码\n")
+var password = readLine()!;
+if(name=="admin"&&password=="888888")
+{
+    print("access permit");
+}
+if(name=="admin"&&password != "888888")
+{
+    print("access denied");
+}
+if(name != "admin")
+{
+    print("用户名不正确")
+}
