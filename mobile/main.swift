29b1b03ea84e056ce479f620400637c51a964263
20181102932 <908907990@qq.com>
2019-09-06 08:50:13 +0800

    Update main.swift

--- mobile/main.swift
+++ mobile/main.swift
@@ -1,4 +1,5 @@
-print("请输入用户名\n")
+import Cocoa
+/*print("请输入用户名\n")
 var name = readLine()!;
 print("请输入密码\n")
 var password = readLine()!;
@@ -13,4 +14,31 @@ if(name=="admin"&&password != "888888")
 if(name != "admin")
 {
     print("用户名不正确")
+}
+
+
+ print("请输入用户名")
+ var username=readline()!
+ print("请输入密码")
+ var password=readline()!
+ if username == "admin"{
+ if password == "888888"
+ print("正确")
+ else
+ print("错误")
+ }
+ else{
+ print("用户名不正确")
+ }
+ */
+var index = 0
+switch index {
+case 100 :
+    print("index 的值为 100")
+case 10,15 :
+    print("index的值为 10 或 15")
+case 5:
+    print("index的值为 5")
+default :
+    print("默认 case")
 }
